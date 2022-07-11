21a613c076837c164ad402fabf6d2dcfe2ffa8f9
Tasha Hankewych <thankewych@gmail.com>
2022-07-10 22:17:25 -0500

    Track the state of the current row/tile and add a keydown listener to insert letters in tiles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,88 @@
+import { useEffect, useRef, useState } from 'react'
 import Board from './components/Board'
 import Header from './components/Header'
 
 function App() {
+  const [rows, _setRows] = useState([]);
+  const [currentRowIndex, _setCurrentRowIndex] = useState(1);
+  const [currentTileIndex, _setCurrentTileIndex] = useState(0);
+
+  const rowsRef = useRef(rows);
+  const setRows = (data) => {
+    rowsRef.current = data;
+    _setRows(data);
+  }
+
+  const currentRowIndexRef = useRef(currentRowIndex);
+  const setCurrentRowIndex = (data) => {
+    currentRowIndexRef.current = data;
+    _setCurrentRowIndex(data);
+  }
+
+  const currentTileIndexRef = useRef(currentTileIndex);
+  const setCurrentTileIndex = (data) => {
+    currentTileIndexRef.current = data;
+    _setCurrentTileIndex(data);
+  }
+
+  const Row = (tiles) => {
+    return {
+      tiles : tiles || getDefaultTiles(),
+    }
+  }
+  const Tile = (letter, state) => {
+    return {
+      letter : letter || '',
+      state : state || 'tbd',
+    }
+  }
+
+  useEffect(() => {
+    const initRows = () => {
+      setRows([row1Tiles, Row(), Row(), Row(), Row()]);
+    }
+
+    initRows();
+    document.addEventListener('keydown', onKeyDown);
+  }, []);
+
+  const onKeyDown = (e) => {
+    const key = e.key;
+    if (/^[a-z]$/i.test(key)) {
+      console.log(key);
+      const currentRow = rowsRef.current[currentRowIndexRef.current];
+      const currentTile = currentRow.tiles[currentTileIndexRef.current];
+      currentTile.letter = key;
+      setRows([...rowsRef.current]);
+      if (currentTileIndexRef.current < 5) {
+        setCurrentTileIndex(currentTileIndexRef.current + 1);
+      }
+    } else {
+      console.log(`not a letter: ${e.key}`);
+    }
+  }
+
+  const getDefaultTiles = () => {
+    const defaultTiles = [];
+    for (let i = 0; i < 6; i++) {
+      defaultTiles[i] = Tile();
+    }
+    return defaultTiles;
+  }
+
+  const row1Tiles = Row([
+    Tile('w', 'correct'),
+    Tile('o', 'correct'),
+    Tile('n', 'present'),
+    Tile('g', 'absent'),
+    Tile('l', 'present'),
+    Tile('e', 'present'),
+  ]);
+
   return (
     <>
       <Header />
-      <Board />
+      <Board rows={rows} />
     </>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,40 +1,11 @@
 import Row from './Row'
 
-const Board = () => {
-  const Tile = (letter, state) => {
-    return {
-      letter : letter || '',
-      state : state || 'tbd',
-    }
-  }
-
-  const defaultTiles = Array(6).fill(Tile());
-
-  const row1Tiles = [
-    Tile('w', 'correct'),
-    Tile('o', 'correct'),
-    Tile('n', 'present'),
-    Tile('g', 'absent'),
-    Tile('l', 'present'),
-    Tile('e', 'present'),
-  ];
-
-  const row2Tiles = [
-    Tile('w'),
-    Tile('o'),
-    Tile(),
-    Tile(),
-    Tile(),
-    Tile(),
-  ];
-
+const Board = ({ rows }) => {
   return (
     <div className='board'>
-      <Row tiles={row1Tiles} />
-      <Row tiles={row2Tiles} />
-      <Row tiles={defaultTiles} />
-      <Row tiles={defaultTiles} />
-      <Row tiles={defaultTiles} />
+      {rows.map((row, index) => (
+        <Row key={index} tiles={row.tiles} />
+      ))}
     </div>
   )
 }
